b3ff0bdb4eaf7df708dd9cbd03416d77580631f8
bdmanik97 <inmanik97@gmail.com>
2024-02-22 23:31:59 +0600

    navbar final

---
+++ script.js
@@ -0,0 +1,8 @@
+// Javasrcipt call function start here
+$( document ).ready(function() {
+    $(".toggle").click(function(){
+     $(".navbar").slideToggle();
+     $(".bar, .close").toggle();
+    }); 
+ });
+ // Javasrcipt call function end here
