c17ef72bc423c3f2990ed0cfcbc91ed9187619ae
bdmanik97 <inmanik97@gmail.com>
2024-02-23 23:35:08 +0600

    final drowpdown

--- script.js
+++ script.js
@@ -4,5 +4,21 @@ $( document ).ready(function() {
      $(".navbar").slideToggle();
      $(".bar, .close").toggle();
     }); 
+
+    $(".home-item").click(function(){
+     $(".d-list").slideToggle();
+    }); 
+
+    $(".home-item2").click(function(){
+     $(".d-list2").slideToggle();
+    }); 
+
+    $(".products-item").click(function(){
+    $(".dwn-list").slideToggle();
+    }); 
+
+    $(".contact-item").click(function(){
+    $(".dwn2-list").slideToggle();
+     }); 
  });
  // Javasrcipt call function end here
